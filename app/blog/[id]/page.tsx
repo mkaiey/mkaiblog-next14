2c952383abd4f1235fdf87cdf2add09d89292467
mkaiey <mkaigit88@gmail.com>
2024-01-10 03:47:19 +0700

    ignoreDuringBuilds

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,6 +3,35 @@ import React from "react";
 import Image from "next/image";
 import BlogContent from "./components/BlogContent";
 
+// export async function generateStaticParams() {
+// 	const { data: blogs } = await fetch(
+// 		process.env.SITE_URL + "/api/blog?id=*" + "*"
+// 	).then((res) => res.json());
+
+// 	return blogs;
+// }
+
+// export async function generateMetadata({ params }: { params: { id: string } }) {
+// 	const { data: blog } = (await fetch(
+// 		process.env.SITE_URL + "/api/blog?id=" + params.id
+// 	).then((res) => res.json())) as { data: IBlog };
+
+// 	return {
+// 		title: blog?.title,
+// 		authors: {
+// 			name: "Project Pro.",
+// 		},
+// 		openGraph: {
+// 			title: blog?.title,
+// 			url: process.env.SITE_URL + "/blog/" + params.id,
+// 			siteName: "Mkai Blog",
+// 			images: blog?.image_url,
+// 			type: "website",
+// 		},
+// 		keywords: ["mkai blog", "projectpro", "mkaidev"],
+// 	};
+// }
+
 export default async function page({ params }: { params: { id: string } }) {
   const { data: blog } = (await fetch(
     process.env.SITE_URL + "/api/blog?id=" + params.id
